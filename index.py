910c2a7464a408d9b32d3201f9167068453bdb2c
Ajay Raikar <iit2017040@iiita.ac.in>
2019-08-30 08:59:22 +0530

    adding new files

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
-
+# import MySQLdb
 from PyQt5.uic import loadUiType
 
 ui, _ = loadUiType('library.ui')
@@ -12,7 +12,64 @@ class MainApp(QMainWindow, ui):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.Handle_UI_Changes()
+        self.Handle_Buttons()
+        self.pushButton.clicked.connect(self.Open_Day_To_Day_Tab)
+        self.pushButton_2.clicked.connect(self.Open_Books_Tab)
+        self.pushButton_3.clicked.connect(self.Open_Users_Tab)
+        self.pushButton_4.clicked.connect(self.Open_Settings_Tab)
 
+    def Handle_UI_Changes(self):
+        self.Hiding_Themes()
+        self.tabWidget.tabBar().setVisible(False)
+    def Handle_Buttons(self):
+        self.pushButton_5.clicked.connect(self.Show_Themes)
+        self.pushButton_21.clicked.connect(self.Hiding_Themes)
+
+    def Show_Themes(self):
+        self.groupBox_2.show()
+
+    def Hiding_Themes(self):
+        self.groupBox_2.hide()
+
+    ################################################
+    ######### Opening Tabs #########################
+    def Open_Day_To_Day_Tab(self):
+        self.tabWidget.setCurrentIndex(0)
+
+    def Open_Books_Tab(self):
+        self.tabWidget.setCurrentIndex(1)
+
+    def Open_Users_Tab(self):
+        self.tabWidget.setCurrentIndex(2)
+
+    def Open_Settings_Tab(self):
+        self.tabWidget.setCurrentIndex(3)
+
+    ################################################
+    ######### Books #########################
+    def Add_New_Book(self):
+        pass
+
+    def Search_Books(self):
+        pass
+
+    def Edit_Books(self):
+        pass
+
+    def Delete_Books(self):
+        pass
+
+    ################################################
+    ######### users #########################
+    def Add_New_User(self):
+        pass
+
+    def Login(self):
+        pass
+
+    def Edit_User(self):
+        pass
 
 def main():
     app = QApplication(sys.argv)
