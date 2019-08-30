dc359eab13c8a76e3e407297112932d719f70f1c
Ajay Raikar <iit2017040@iiita.ac.in>
2019-08-30 20:45:12 +0530

    Modified index.py

--- index.py
+++ index.py
@@ -14,7 +14,12 @@ class MainApp(QMainWindow, ui):
         self.setupUi(self)
         self.Handle_UI_Changes()
         self.Handle_Buttons()
+        self.Show_Author()
         self.Show_Category()
+        self.Show_Publisher()
+        self.Show_Category_Combobox()
+        self.Show_Author_Combobox()
+        self.Show_Publisher_Combobox()
 
         self.pushButton.clicked.connect(self.Open_Day_To_Day_Tab)
         self.pushButton_2.clicked.connect(self.Open_Books_Tab)
@@ -110,7 +115,7 @@ class MainApp(QMainWindow, ui):
 
         self.cur.execute(''' SELECT category_name FROM category ''')
         data = self.cur.fetchall()
-        print(data)
+        # print(data)
         # self.tableWidget_4.insertRow(0)
         if data:
             self.tableWidget_4.setRowCount(0)
@@ -134,9 +139,25 @@ class MainApp(QMainWindow, ui):
         self.db.commit()
         self.lineEdit_20.setText('')
         self.statusBar().showMessage('New Author Added ')
+        self.Show_Author()
 
     def Show_Author(self):
-        pass
+        self.db = MySQLdb.connect(host='localhost', user='root', password='d33ps3curity', db='library')
+        self.cur = self.db.cursor()
+
+        self.cur.execute(''' SELECT author_name FROM author ''')
+        data = self.cur.fetchall()
+        # self.tableWidget_4.insertRow(0)
+        if data:
+            self.tableWidget_3.setRowCount(0)
+            self.tableWidget_3.insertRow(0)
+            for row, form in enumerate(data):
+                for column, item in enumerate(form):
+                    self.tableWidget_3.setItem(row, column, QTableWidgetItem(str(item)))
+                    column += 1
+
+                row_position = self.tableWidget_3.rowCount()
+                self.tableWidget_3.insertRow(row_position)
 
     def Add_Publisher(self):
         self.db = MySQLdb.connect(host='localhost', user='root', password='d33ps3curity', db='library')
@@ -150,9 +171,56 @@ class MainApp(QMainWindow, ui):
         self.db.commit()
         self.lineEdit_19.setText('')
         self.statusBar().showMessage('New Publisher Added ')
+        self.Show_Publisher()
 
     def Show_Publisher(self):
-        pass
+        self.db = MySQLdb.connect(host='localhost', user='root', password='d33ps3curity', db='library')
+        self.cur = self.db.cursor()
+
+        self.cur.execute(''' SELECT publisher_name FROM publisher ''')
+        data = self.cur.fetchall()
+        # self.tableWidget_4.insertRow(0)
+        if data:
+            self.tableWidget_2.setRowCount(0)
+            self.tableWidget_2.insertRow(0)
+            for row, form in enumerate(data):
+                for column, item in enumerate(form):
+                    self.tableWidget_2.setItem(row, column, QTableWidgetItem(str(item)))
+                    column += 1
+
+                row_position = self.tableWidget_2.rowCount()
+                self.tableWidget_2.insertRow(row_position)
+
+    ################################################
+    ######### show settings data in UI #########################
+
+    def Show_Category_Combobox(self):
+        self.db = MySQLdb.connect(host='localhost', user='root', password='d33ps3curity', db='library')
+        self.cur = self.db.cursor()
+
+        self.cur.execute(''' SELECT category_name FROM category ''')
+        data = self.cur.fetchall()
+
+        # print(data)
+        for category in data:
+            self.comboBox_3.addItem(category[0])
+
+    def Show_Author_Combobox(self):
+        self.db = MySQLdb.connect(host='localhost', user='root', password='d33ps3curity', db='library')
+        self.cur = self.db.cursor()
+        self.cur.execute(''' SELECT author_name FROM author ''')
+        data = self.cur.fetchall()
+        for author in data:
+            self.comboBox_4.addItem(author[0])
+    def Show_Publisher_Combobox(self):
+        self.db = MySQLdb.connect(host='localhost', user='root', password='d33ps3curity', db='library')
+        self.cur = self.db.cursor()
+        self.cur.execute(''' SELECT publisher_name FROM publisher ''')
+        data = self.cur.fetchall()
+
+        for publisher in data:
+            self.comboBox_5.addItem(publisher[0])
+
 def main():
     app = QApplication(sys.argv)
     window = MainApp()
